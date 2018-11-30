cdcdcc65645e7938b07f97cd6de6d6aa964e80c4
Apple <apple@Apples-iMac.local>
2018-11-30 23:29:02 +0530

    project setup

---
+++ SwiftScrollView/SwiftScrollView/SwiftScrollView.swift
@@ -0,0 +1,25 @@
+//
+//  SwiftScrollView.swift
+//  SwiftScrollView
+//
+//  Created by Apple on 30/11/18.
+//  Copyright © 2018 Rajamohan-S. All rights reserved.
+//
+
+open class SwiftScrollView:UIScrollView,UITextFieldDelegate,UITextViewDelegate{
+    
+    private var notification = NotificationCenter.default
+    
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+
+        self.notification.removeObserver(self)
+    }
+}
---
+++ SwiftScrollViewExample/SwiftScrollViewExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftScrollViewExample
+//
+//  Created by Apple on 30/11/18.
+//  Copyright © 2018 Rajamohan-S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
